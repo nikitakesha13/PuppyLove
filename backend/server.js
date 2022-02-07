0a1dab2beee585ade552c02dc7b2440f06571f1b
Nikita Udoenko <nudodenko99>
2022-02-06 22:11:42 -0600

    three

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const cors = require('cors');
-const mongoose = require('mongoose');
+import express, { json, static } from "express";
+import cors from 'cors';
+import { connect, connection as _connection } from 'mongoose';
 
 require('dotenv').config();
 
@@ -8,22 +8,22 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(json());
 
 const uri = process.env.ATLAS_URI;
 console.log(uri);
 
-mongoose.connect(uri);
-const connection = mongoose.connection;
+connect(uri);
+const connection = _connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-const usersRouter = require('./routes/users');
-const dogRouter = require('./routes/dogs');
+import usersRouter from './routes/users';
+import dogRouter from './routes/dogs';
 app.use('/users', usersRouter);
 app.use('/dogs', dogRouter);
-app.use(express.static('./frontend/public/'));
+app.use(static('./frontend/public/'));
 
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
