63b137d607f2346b3b168be7cfeb47755ae4cf02
Nikita Udoenko <nudodenko99>
2022-02-06 22:29:50 -0600

    t

--- backend/server.js
+++ backend/server.js
@@ -14,17 +14,7 @@ app.use(express.json());
 const dirname = path.resolve();
 console.log(dirname);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(dirname, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+app.use(express.static(path.join(dirname, "/frontend/build")));
 
 const uri = process.env.ATLAS_URI;
 console.log(uri);
