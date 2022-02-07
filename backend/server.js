4b086e590ac60b648f52ee6786f7c7f4d5f8aa92
Nikita Udoenko <nudodenko99>
2022-02-06 22:24:58 -0600

    well

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -9,6 +10,21 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const dirname = path.resolve();
+console.log(dirname);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 const uri = process.env.ATLAS_URI;
 console.log(uri);
@@ -23,7 +39,7 @@ const usersRouter = require('./routes/users');
 const dogRouter = require('./routes/dogs');
 app.use('/users', usersRouter);
 app.use('/dogs', dogRouter);
-app.use(express.static('./frontend/public/'));
+// app.use(express.static('./frontend/public/'));
 
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
